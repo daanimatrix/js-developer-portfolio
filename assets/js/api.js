970a41dca803598c6db25849ce00dcd4ad836dbd
Emerson Romana Senna <133699469+Minfermis@users.noreply.github.com>
2023-11-06 13:35:13 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,7 +1,17 @@
-
 async function fetchProfileData() {
     const url = 'https://api.github.com/repos/Minfermis/js-developer-portfolio/contents/data/profile.json';
-    const response = await fetch(url)
-    const profileData = await response.json()
-    return profileData
+    try {
+        const response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            // Decodifica os dados, pois estão em base64
+            const profileData = JSON.parse(atob(data.content));
+            return profileData;
+        } else {
+            throw new Error('Erro ao obter os dados');
+        }
+    } catch (error) {
+        console.error('Erro:', error);
+        throw error;
+    }
 }
