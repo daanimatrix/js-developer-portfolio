942e48133191935a5ed2582d91b4aa8b00c6c338
Emerson Romana Senna <133699469+Minfermis@users.noreply.github.com>
2023-11-06 13:49:03 -0300

    Update api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData() {
-    const url = 'https://avatars.githubusercontent.com/u/133699469?s=400&u=1b688bc80bbe9e9a3ef378000b18d89c9b8e3f01&v=4';
+    const url = 'https://api.github.com/repos/Minfermis/js-developer-portfolio/contents/data/profile.json';
     try {
         const response = await fetch(url);
         if (response.ok) {
